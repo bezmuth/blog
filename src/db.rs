d20b6efdfae0a77e8224844ea446c0940a93b141
bezmuth <benkel97@protonmail.com>
2025-06-24 01:40:34 +0100

    Add blogposts on homepage

--- src/db.rs
+++ src/db.rs
@@ -3,26 +3,23 @@ use chrono::{DateTime, Local, NaiveDateTime, TimeZone, Utc};
 use scraper::{Html, Selector};
 use sled::Db;
 use std::fs;
-use std::path::PathBuf;
 use std::str;
 
-// Post metadata is stored in a database alongside the posts because if I cannot
+// Post metadata is stored in a database alongside the posts because I cannot
 // know the timezone the blogposts were written in, we pull from the local
 // timezone when the posts were added instead and then store that in the
-// database
+// database. This is so I dont break any atom readers
 
 #[derive(Debug, Clone, Encode, Decode)]
-pub struct Post {
+struct Post {
     pub name: String,
     date_time: String,
-    pub path: std::path::PathBuf,
 }
 impl Post {
-    pub fn new(name: String, date_time: chrono::DateTime<Utc>, path: std::path::PathBuf) -> Self {
-        Post {
+    fn new(name: String, date_time: chrono::DateTime<Utc>) -> Self {
+        Self {
             name,
             date_time: date_time.to_rfc3339(),
-            path,
         }
     }
 }
@@ -33,20 +30,19 @@ pub struct Metadata {
 }
 impl Metadata {
     pub fn new() -> Result<Self, sled::Error> {
-        let meta = Metadata {
+        let meta = Self {
             db: sled::open("posts/metadata")?,
         };
         let post_paths = fs::read_dir("posts").unwrap();
 
         for path in post_paths {
-            // get the path
-            let path = path.unwrap().path();
-            meta.add_post(path)
+            let filename = path.unwrap().file_name().to_string_lossy().to_string();
+            meta.add_post(filename);
         }
         Ok(meta)
     }
 
-    pub fn get_post(self, filename: &str) -> Option<Post> {
+    fn get_post(self, filename: &str) -> Option<Post> {
         if let Ok(Some(bins)) = self.db.get(filename) {
             Some(
                 bincode::decode_from_slice(&bins[..], bincode::config::standard())
@@ -57,20 +53,28 @@ impl Metadata {
             None
         }
     }
+    pub fn get_post_title(self, filename: &str) -> Option<String> {
+        if let Some(post) = self.get_post(filename) {
+            Some(post.name)
+        } else {
+            None
+        }
+    }
 
-    pub fn add_post(&self, path: PathBuf) {
-        // get the filename
-        let filename = path.file_name().unwrap().to_string_lossy().to_string();
+    /// Two assumptions are made about post files:
+    /// 1. They are valid(ish) html exported from orgmode
+    /// 2. The filenames are valid rust strings
+    pub fn add_post(&self, filename: String) {
         if filename.ends_with("html") && !self.db.contains_key(&filename).unwrap() {
             // read the file
-            let contents = fs::read_to_string(&path).unwrap();
+            let contents = fs::read_to_string(format!("posts/{filename}")).unwrap();
             let fragment = Html::parse_fragment(&contents);
 
             // parse title
             let mut title = String::new();
             let title_selector = Selector::parse("title").unwrap();
             for element in fragment.select(&title_selector) {
-                title = element.inner_html().to_string()
+                title = element.inner_html().to_string();
             }
 
             // parse date
@@ -85,7 +89,7 @@ impl Metadata {
                 date = local_date.to_utc();
             }
 
-            let post = Post::new(title, date, path.to_path_buf());
+            let post = Post::new(title, date);
             let bins = bincode::encode_to_vec(post, bincode::config::standard()).unwrap();
             self.db.insert(filename, bins).unwrap();
         }
@@ -100,7 +104,12 @@ impl Metadata {
     /// Passing a None to the format string formats the datetime in rfc3339
     pub fn get_posts_sorted(self, format: Option<&str>) -> Vec<(String, String, String)> {
         // I was a bit worried about how long this function would take, but its
-        // only a couple of hundered microseconds on --release
+        // only a couple of hundered microseconds on --release with 20 test
+        // files. I should really just generate this whenever I add a new file
+        // but for now its fine, a bit wasteful but fine. Or I could use a
+        // proper database but I think thats overkill
+
+        // First, convert the entire db into a vector so it can be sorted
         let mut posts: Vec<_> = self
             .db
             .into_iter()
@@ -112,20 +121,31 @@ impl Metadata {
                     .0;
                 // convert the string into datetime so we can easily sort
                 let date_time = chrono::DateTime::parse_from_rfc3339(&post.date_time).unwrap();
-                (filename, post.name, date_time, post.path)
+                // convert the filename key back into a string
+                (
+                    String::from_utf8_lossy(&filename).to_string(),
+                    post.name,
+                    date_time,
+                )
             })
             .collect();
 
+        // sort the posts
         posts.sort_by(|a, b| b.2.cmp(&a.2));
 
         posts
             .into_iter()
-            .map(|(filename, name, date_time, _)| {
+            .map(|(filename, name, date_time)| {
+                // convert datetime to requested format
                 let date = match format {
                     Some(fmt) => date_time.format(fmt).to_string(),
                     None => date_time.to_rfc3339(),
                 };
-                (String::from_utf8_lossy(&filename).to_string(), name, date) // sketchy
+                format.map_or_else(
+                    || date_time.to_rfc3339(),
+                    |fmt| date_time.format(fmt).to_string(),
+                );
+                (filename, name, date) // sketchy
             })
             .collect()
     }
--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,15 @@ mod db;
 //  I've shifted to storing the post metadata in a database, i can just watch
 //  the posts folder now but I'm not sure if theres any need
 //
-// TODO: watch the posts dir for any changes to add new blogposts
 //
 // NOTES: There is a lot of unwrap usage in this program, as I'm the only one
 // using it and I want to be able to understand failures (i.e. I messed up
 // something in the templates), in more business ready software I'd probably
 // just return a 500 but I dont need to worry about that.
+//
+// TODO: add github activity and recent blogposts to my home page, maybe split these into two panes?
+// TODO: regenerate sorted posts only on adding a blogpost
+// TODO: watch the posts dir for any changes to add new blogposts
 
 struct AppState {
     env: Environment<'static>,
@@ -65,10 +68,13 @@ async fn main() {
 async fn handler_home(State(state): State<Arc<AppState>>) -> Result<Html<String>, StatusCode> {
     let template = state.env.get_template("home").unwrap();
 
+    let entries = &state.metadata.clone().get_posts_sorted(Some("%Y-%m-%d"))[0..10];
+
     let rendered = template
         .render(context! {
             title => "Bezmuth",
-            welcome_text => "Software dev, open source contributor, writer of blogs, destroyer of worlds, etc etc",
+            welcome_text => "Software dev, open source contributor, writer of blogs, div centerer, destroyer of worlds, etc etc",
+            entries,
         })
         .unwrap();
 
@@ -99,7 +105,7 @@ async fn handler_feed(State(state): State<Arc<AppState>>) -> Result<String, Stat
 
     let mut last_post_date = String::new();
     if let Some(last_post) = entries.first() {
-        last_post_date = last_post.2.clone();
+        last_post_date.clone_from(&last_post.2);
     }
 
     let rendered = template
@@ -115,15 +121,16 @@ async fn handler_blog_post(
     State(state): State<Arc<AppState>>,
     Path(filename): Path<String>,
 ) -> Result<Html<String>, StatusCode> {
-    if let Some(post) = state.metadata.clone().get_post(&filename) {
+    // this just verifies the requested file exists in the database
+    if let Some(title) = state.metadata.clone().get_post_title(&filename) {
         let template = state.env.get_template("blogpost").unwrap();
-        let post_content = fs::read_to_string(post.clone().path)
+        let post_content = fs::read_to_string(format!("posts/{filename}"))
             .await
             .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
         let rendered = template
             .render(context! {
-                title => post.name,
+                title,
                 blog_post => post_content,
             })
             .unwrap();
